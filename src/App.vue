07f1f51be842e38f3a17ff7907f48a25c4d346ee
Antonio Gil <antonioluisgil@gmail.com>
2018-05-19 20:29:36 +0200

    Add icons

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <svg class="icon">
+      <use xlink:href="#menu"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="./assets/icons.svg#menu"></use>
+    </svg>
     <img src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -18,7 +24,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
